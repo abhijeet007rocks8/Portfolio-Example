1a35888476946f6f45955a2ae7b1b74109664a01
Abhijeet Chatterjee <abhijeet007rocks8@gmail.com>
2021-09-10 08:36:59 +0530

    Portfolio files added

---
+++ profile-frontend/src/data/config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  // TODO: change your profile information here
+  name: "Abhijeet Chatterjee",
+  greeting: "Hey There!! 👋",
+  greetingDescription: "I'm Abhijeet Chattejee I'm a Software Engineer Competitive Coder & Web Developer !!",
+  githubUrl: "https://github.com/abhijeet007rocks8",
+  linkedinUrl: "https://www.linkedin.com/in/abhijeet-chatterjee-445aa2195/",
+  cvLink: "https://drive.google.com/file/d/1RodfVImyd2d5B2a9nLOzKw0Cmy1mJqIY/view",
+};
